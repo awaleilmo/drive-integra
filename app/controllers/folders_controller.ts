d3d049b95eca3972e05c421a50b08b30de48ecc2
Awal Eilmo <awal.eilmo@gmail.com>
2024-08-30 16:29:20 +0700

    folder controller and validator create

--- app/controllers/folders_controller.ts
+++ app/controllers/folders_controller.ts
@@ -1,4 +1,9 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import type { HttpContext } from '@adonisjs/core/http'
 
 export default class FoldersController {
+  async addFolder(ctx: HttpContext) {
+    const userId = ctx.auth.user?.id
+
+    return ctx.inertia.render('addFolder')
+  }
 }
